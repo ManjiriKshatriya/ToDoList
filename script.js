cbae6897dd484b2c5c86c15cf1d5d9c5316d79ca
Manjiri Kshatriya <95565656+ManjiriKshatriya@users.noreply.github.com>
2022-08-14 23:36:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.querySelector('#push').onclick = function()
+{
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert('Please Enter a task');
+
+    }
+    else{
+        document.querySelector('#tasks').innerHTML += 
+        `
+            <div class ="task">
+                <span id="taskname">
+                    ${document.querySelector('#newtask input').value}
+                </span>
+
+                <button class="delete">
+                <i class="far fa-trash-alt"></i>
+                </button>
+
+            </div>
+
+        `;
+
+        var curr_tasks = document.querySelectorAll(".delete");
+        for (var i =0; i<curr_tasks.length;i++){
+            curr_tasks[i].onclick = function(){
+                this.parentNode.remove();
+            }
+        }
+
+        var tasks = document.querySelectorAll(".task");
+        for(var i =0;i<tasks.length;i++){
+            tasks[i].onclick = function(){
+                this.classList.toggle('Completed');
+            }
+        }
+
+        document.querySelector("#newtask input").value = "";
+    }
+}
